eea9269e826c7a431c8c27a00ce19e1c3134fd56
jenifer <shennifer06@gmail.com>
2021-08-12 23:59:32 +0200

    primer commit

---
+++ script.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', function(){
+
+
+    let yes = document.querySelector('.btn-yes');
+    const no = document.querySelector('.no');
+
+no.addEventListener('click', function(){
+            width = window.innerWidth - 150;
+            height = window.innerHeight - 150;
+
+            newWidth = (Math.random() * width);
+            newHeight = (Math.random() * height);
+
+            document.getElementById('btnNo').style.position = "absolute";
+            document.getElementById('btnNo').style.left = newWidth + "px";
+            document.getElementById('btnNo').style.top = newHeight + "px";
+})
+
+yes.addEventListener('click', function(){
+    Swal.fire({
+        html:
+        '<div class="amor"><h2>Te quiero Lux!!</h2> <div class="amor2"><img class="love" src="image/amor.gif" alt="love"></div> <div class="frase"><h2>gracias por ser tan adorable conmigo</h2> </div> </div> ',
+        confirmButtonText: `Aceptar`
+    })
+})
+    
+})
